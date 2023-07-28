e3e8bd9562bf88ba932420641955c3c157f0853d
Adam Wojciak <Code.AdamW78@gmail.com>
2023-07-28 17:19:26 -0400

    Initial commit

---
+++ src/main/java/org/awdevelopment/armorunderwear/ArmorUnderwearFabric.java
@@ -0,0 +1,13 @@
+package org.awdevelopment.armorunderwear;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArmorUnderwearFabric implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
